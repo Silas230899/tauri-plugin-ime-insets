2c3d8478bb2a844e9b420a9493cf94b854e051f9
Silas2308 <silas.michael.hofmann@mni.thm.de>
2026-09-22 15:03:32 +0200

    Fix more package names

--- android/src/androidTest/java/ExampleInstrumentedTest.kt
+++ android/src/androidTest/java/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package de.hofmann.ime-insets
+package de.hofmann.ime_insets
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("de.hofmann.ime-insets", appContext.packageName)
+        assertEquals("de.hofmann.ime_insets", appContext.packageName)
     }
 }
